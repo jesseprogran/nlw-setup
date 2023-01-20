e020b5be9dfb94d5d95a0778913adfaf9ef21a82
Jessé Silva <jessepescador22@gmail.com>
2023-01-19 21:34:14 -0300

    atribuindo animações ao css com o before e o houver

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(form);
+
+const data = {
+  run: [
+    "01-01",
+    "01-02",
+    "01-03",
+    "01-04",
+    "01-05",
+    "01-06",
+    "01-07",
+    "01-08",
+    "01-09",
+    "01-010",
+  ],
+  takePills: ["01-03"],
+  journal: ["01-02"],
+}
+
+nlwSetup.setData(data);
+nlwSetup.load();
